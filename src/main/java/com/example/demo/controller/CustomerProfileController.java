3610c1228224b6ad6c3d0397c6945d8bd06e8a82
bala-amypo <bala@amypo.in>
2025-12-20 05:47:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/CustomerProfileController.java
+++ src/main/java/com/example/demo/controller/CustomerProfileController.java
@@ -4,34 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.example.demo.entity.StudentEntity;
-import com.example.demo.service.StudentService;
+import com.example.demo.entity.CustomerProfile;
+import com.example.demo.service.CustomerProfileService;
 import java.util.List;
 
 @RestController
-public class StudentController{
-    @Autowired StudentService ser;
+public class CustomerProfileController{
+    @Autowired CustomerProfileService ser;
 @PostMapping("/post")
-public StudentEntity dopost(@RequestBody StudentEntity stu){
+public CustomerProfile dopost(@RequestBody CustomerProfile stu){
     return ser.postdata(stu);
 }
 @GetMapping("/get")
-public List<StudentEntity>getval(){
+public List<CustomerProfile>getval(){
     return ser.getAllData();
 }
-@DeleteMapping("/delete/{id}")
-public String deleteval(@PathVariable int id){
-    return ser.deleteData(id); 
-}
 @GetMapping("/getid/{id}")
-public StudentEntity getdataid(@PathVariable int id){
+public CustomerProfile getdataid(@PathVariable int id){
    return ser.getData(id);
 }
 @PutMapping ("/put/{id}")
-public StudentEntity putval(@PathVariable int id, @RequestBody StudentEntity entity){
+public CustomerProfile putval(@PathVariable int id, @RequestBody CustomerProfile entity){
  return ser.updateData(id,entity);
 }
 }

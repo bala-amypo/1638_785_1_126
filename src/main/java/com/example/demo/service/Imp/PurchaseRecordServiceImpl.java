a3dfebc642708f1a387d4569f0602bcb5c39e818
bala-amypo <bala@amypo.in>
2025-12-20 14:47:00 +0000

    User commit

--- src/main/java/com/example/demo/service/Imp/PurchaseRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/Imp/PurchaseRecordServiceImpl.java
@@ -11,8 +11,12 @@ public class PurchaseRecordServiceImpl implements PurchaseRecordService{
       @Autowired PurchaseRecordRepository purchaseRecordRepository;
      
       @Override
- public PurchaseRecord postdata(PurchaseRecord stu){
-    return purchaseRecordRepository.save(stu);
+ public PurchaseRecord recordPurchase(PurchaseRecord purchase){
+    return purchaseRecordRepository.save(purchase);
+ }
+ @Override
+ public List<PurchaseRecord> getAllData(){
+      return purchaseRecordRepository.findAll();
  }
  @Override
  public List<PurchaseRecord> getAllData(){
@@ -21,13 +25,5 @@ public class PurchaseRecordServiceImpl implements PurchaseRecordService{
  @Override
  public  PurchaseRecord getData(int id){
     return purchaseRecordRepository.findById(id).orElse(null);
- }
- @Override
- public PurchaseRecordupdateData(int id,StudentEntity entity){
-    if(purchaseRecordRepository.existsById(id)){
-        entity.setId(id);
-        return student.save(entity);
-    }
-     return null;
  }
 }

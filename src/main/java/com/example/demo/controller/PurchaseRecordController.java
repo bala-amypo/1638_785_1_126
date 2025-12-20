5bab6d6d8f936961d8d1374d822fa45e59166e9f
bala-amypo <bala@amypo.in>
2025-12-20 14:56:00 +0000

    User commit

--- src/main/java/com/example/demo/controller/PurchaseRecordController.java
+++ src/main/java/com/example/demo/controller/PurchaseRecordController.java
@@ -4,34 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.example.demo.entity.Purchase;
-import com.example.demo.service.StudentService;
+import com.example.demo.entity.PurchaseRecord;
+import com.example.demo.service.PurchaseRecordService;
 import java.util.List;
 
 @RestController
-public class StudentController{
-    @Autowired StudentService ser;
-@PostMapping("/post")
-public StudentEntity dopost(@RequestBody StudentEntity stu){
-    return ser.postdata(stu);
+public class PurchaseRecordController{
+    @Autowired PurchaseRecordService purchaseRecordService;
+@PostMapping("/api/purchases")
+public PurchaseRecord dopost(@RequestBody PurchaseRecord stu){
+    return purchaseRecordService.postdata(stu);
 }
 @GetMapping("/get")
-public List<StudentEntity>getval(){
-    return ser.getAllData();
-}
-@DeleteMapping("/delete/{id}")
-public String deleteval(@PathVariable int id){
-    return ser.deleteData(id); 
+public List<PurchaseRecord>getval(){
+    return purchaseRecordService.getAllData();
 }
 @GetMapping("/getid/{id}")
-public StudentEntity getdataid(@PathVariable int id){
-   return ser.getData(id);
+public PurchaseRecord getdataid(@PathVariable int id){
+   return purchaseRecordService.getData(id);
 }
 @PutMapping ("/put/{id}")
-public StudentEntity putval(@PathVariable int id, @RequestBody StudentEntity entity){
- return ser.updateData(id,entity);
+public PurchaseRecord putval(@PathVariable int id, @RequestBody PurchaseRecord entity){
+ return purchaseRecordService.updateData(id,entity);
 }
 }

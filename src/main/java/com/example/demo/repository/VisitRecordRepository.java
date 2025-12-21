cfd49d26fc19a4da0a05910175e9e3148fc6f849
bala-amypo <bala@amypo.in>
2025-12-21 04:34:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/VisitRecordRepository.java
+++ src/main/java/com/example/demo/repository/VisitRecordRepository.java
@@ -3,8 +3,6 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.VisitRecord;
 import java.util.List;
-public interface VisitRecordRepository
-        extends JpaRepository<VisitRecord, Long> {
-
+public interface VisitRecordRepository extends JpaRepository<VisitRecord, Long> {
     List<VisitRecord> findByCustomerId(long customerId);
 }

15b7390a3e1d2337d64551ffa6eab82de4926131
bala-amypo <bala@amypo.in>
2025-12-21 03:48:20 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/VisitRecordController.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity ;
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+ public class PurchaseRecord{
+     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private long customerId;
+    private LocalDate visitDate;
+    private String channel;
+ }
+ 

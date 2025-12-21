03aefd0fddee1eee270b83697d36da3000daf9bb
bala-amypo <bala@amypo.in>
2025-12-21 04:02:53 +0000

    User commit

--- src/main/java/com/example/demo/service/VisitRecordService.java
+++ src/main/java/com/example/demo/service/VisitRecordService.java
@@ -3,8 +3,8 @@ import com.example.demo.entity.VisitRecord;
 import java.util.List;
 public interface VisitRecordService{
     VisitRecord recordVisit(VisitRecord visit);
-    List<VisitRecord> getAllData();
-    StudentEntity getData(int id);
-    StudentEntity updateData(int id,StudentEntity entity);
+    List<VisitRecord> getAllVisits();
+    List<VisitRecord> getVisitsByCustomer(long customerId);
+    VisitRecord getVisitById(Long id);
 }
 

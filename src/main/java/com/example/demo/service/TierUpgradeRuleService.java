d196d284d266d2a58beff6bfd9dc0313539f043f
bala-amypo <bala@amypo.in>
2025-12-21 04:49:06 +0000

    User commit

--- src/main/java/com/example/demo/service/TierUpgradeRuleService.java
+++ src/main/java/com/example/demo/service/TierUpgradeRuleService.java
@@ -3,9 +3,8 @@ import com.example.demo.entity.TierUpgradeRule;
 import java.util.List;
 public interface TierUpgradeRuleService{
    TierUpgradeRule createRule(TierUpgradeRule rule);
-   TierUpgradeRule updateRule(Long id,TierUpgradeRule entity);
-    List<TierUpgradeRule> getAllData();
-    String deleteData(int id);
-    TierUpgradeRule getData(int id);
-    
+   TierUpgradeRule updateRule(Long id,TierUpgradeRule updatedRule);
+    List<TierUpgradeRule> getActiveRules();
+    List<TierUpgradeRule> getAllRules();
+    TierUpgradeRule getRule(String fromTier, String toTier);
 }
